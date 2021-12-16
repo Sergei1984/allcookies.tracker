d5d603e48bbcce6b669c4dbea2941aba552974d4
Serhii Tokariev <seregat1984@gmail.com>
2021-12-16 22:13:04 +0200

    Update publish script

--- allcookies-tracker-backend/publish.sh
+++ allcookies-tracker-backend/publish.sh
@@ -5,25 +5,26 @@ GIT_COMMIT=$(git show -s --format=%H)
 GIT_BRANCH=$(git branch --show-current)
 REG_USER=antx
 REG_PWD=Qwe344Jklld09
-export RELEASE=${GIT_COMMIT}
+# export RELEASE=${GIT_COMMIT}
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-export KUBECONFIG="${DIR}/kubeconfig"
+export KUBECONFIG="${DIR}/k8s/kubeconfig"
 
-DOCKER_COMPOSE_FILE="${DIR}/app/docker-compose.yml"
+# DOCKER_COMPOSE_FILE="${DIR}/docker-compose.yml"
 
-docker login -u $REG_USER -p $REG_PWD $DOCKER_REGISTRY
+# docker login -u $REG_USER -p $REG_PWD $DOCKER_REGISTRY
 
-docker-compose -f "${DIR}/app/docker-compose.yml" build --progress plain && \
-docker-compose -f "${DIR}/app/docker-compose.yml" push
+# docker-compose -f "${DIR}/app/docker-compose.yml" build --progress plain && \
+# docker-compose -f "${DIR}/app/docker-compose.yml" push
 
-if [[ $? == "1" ]]
-then
-    echo "Error building ${S}"
-    exit 1
-fi
+# if [[ $? == "1" ]]
+# then
+#     echo "Error building ${S}"
+#     exit 1
+# fi
+
 
 helm upgrade \
     --install \
@@ -37,6 +38,6 @@ helm upgrade \
     --create-namespace \
     --description "Commit ${GIT_COMMIT} branch ${GIT_BRANCH}" \
     allcookies-tracker \
-    "${DIR}/app/allcookies-tracker"
+    "${DIR}/k8s/app/allcookies-tracker"
 
 kubectl rollout restart deployment -n allcookies-tracker
