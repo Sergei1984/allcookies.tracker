26680edc5432246a70b54f0b37acd11ab3e52be6
Serhii Tokariev <seregat1984@gmail.com>
2021-12-16 22:05:00 +0200

    Kubernetes helm chart

---
+++ allcookies-tracker-backend/k8s/publish.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOCKER_REGISTRY=docker-registry.a-dev.com
+GIT_COMMIT=$(git show -s --format=%H)
+GIT_BRANCH=$(git branch --show-current)
+REG_USER=antx
+REG_PWD=Qwe344Jklld09
+export RELEASE=${GIT_COMMIT}
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export KUBECONFIG="${DIR}/kubeconfig"
+
+DOCKER_COMPOSE_FILE="${DIR}/app/docker-compose.yml"
+
+docker login -u $REG_USER -p $REG_PWD $DOCKER_REGISTRY
+
+docker-compose -f "${DIR}/app/docker-compose.yml" build --progress plain && \
+docker-compose -f "${DIR}/app/docker-compose.yml" push
+
+if [[ $? == "1" ]]
+then
+    echo "Error building ${S}"
+    exit 1
+fi
+
+helm upgrade \
+    --install \
+    --namespace allcookies-tracker \
+    --set "imageLabel=${RELEASE:-latest}" \
+    --set "deployInfo.branch=${GIT_BRANCH:-$RELEASE}" \
+    --set "deployInfo.commit=${RELEASE}" \
+    --set "deployInfo.deployedBy=$(whoami)" \
+    --set "deployInfo.deployedAt=$(date)" \
+    --set "deployInfo.deployedFrom=$(hostname)" \
+    --create-namespace \
+    --description "Commit ${GIT_COMMIT} branch ${GIT_BRANCH}" \
+    allcookies-tracker \
+    "${DIR}/app/allcookies-tracker"
+
+kubectl rollout restart deployment -n allcookies-tracker
