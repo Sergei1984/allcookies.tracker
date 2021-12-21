136b64e326f8c843febfa4ef6cdd46b4d159c739
a33nazarenko <a33.nazarenko@gmail.com>
2021-12-21 12:29:51 +0200

    added mock product counter, image picker, shop list

--- allcookies-tracker-frontend/src/screens/HomeScreen/HomeScreen.tsx
+++ allcookies-tracker-frontend/src/screens/HomeScreen/HomeScreen.tsx
@@ -5,18 +5,106 @@ import {
   useTheme,
 } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { AppText } from "../../components/AppText";
+import ProductCount from "../../components/ProductCount/ProductCount";
 import createStyles from "./styles";
-
+import ImagePicker from "react-native-image-crop-picker";
+import { useGetImage } from "../../hooks/useGetImage";
 interface IProps {
   navigation: NavigationProp<ParamListBase>;
 }
 const HomeScreen: React.FC<IProps> = ({ navigation }) => {
+  const dataCount = [
+    { name: "test1", count: 3 },
+    { name: "test2", count: 7 },
+    { name: "test3", count: 9 },
+  ];
+
+  const dataPoints = [
+    { id: "1", name: "Point1" },
+    { id: "2", name: "Point2" },
+    { id: "3", name: "Point3" },
+  ];
+  const { data, handle } = useGetImage();
   const styles = React.useMemo(() => createStyles(), []);
   const { colors } = useTheme();
+  const [products, setProducts] = React.useState(dataCount);
+
+  const handleIncrement = React.useCallback(
+    (name) => {
+      let newProduct = products.map((el) =>
+        el.name === name ? { ...el, count: el.count + 1 } : el
+      );
+      setProducts(newProduct);
+    },
+    [products]
+  );
+
+  const handleDecrement = React.useCallback(
+    (name) => {
+      let newProduct = products.map((el) =>
+        el.name === name
+          ? { ...el, count: el.count !== 0 ? el.count - 1 : 0 }
+          : el
+      );
+      setProducts(newProduct);
+    },
+    [products]
+  );
+
+  const renderShopPoints = () => {
+    const renderItem = ({ item }: any) => {
+      return (
+        <TouchableOpacity style={styles.renderItemPoint}>
+          <AppText>{item.name}</AppText>
+        </TouchableOpacity>
+      );
+    };
+
+    return (
+      <View style={styles.pointsWrapper}>
+        <FlatList
+          data={dataPoints}
+          renderItem={renderItem}
+          horizontal={true}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.body}>
-      <Text style={[styles.text, { color: colors.text }]}>Home Screen</Text>
+      {/* Magazine points */}
+      {renderShopPoints()}
+      {/* Count of products */}
+      {products.map((item, index) => {
+        return (
+          <ProductCount
+            key={"product" + index}
+            name={item.name}
+            count={item.count}
+            handleIncrement={handleIncrement}
+            handleDecrement={handleDecrement}
+          />
+        );
+      })}
+      <TouchableOpacity onPress={() => handle.pickSingle(true)}>
+        {data.image ? (
+          <Image style={styles.avatar} source={data.image} />
+        ) : (
+          <AppText>Choose photo</AppText>
+        )}
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => handle.pickSingleWithCamera(true)}>
+        {data.image ? (
+          <Image style={styles.avatar} source={data.image} />
+        ) : (
+          <AppText>Open camera</AppText>
+        )}
+      </TouchableOpacity>
     </View>
   );
 };
--- allcookies-tracker-frontend/src/screens/HomeScreen/styles.ts
+++ allcookies-tracker-frontend/src/screens/HomeScreen/styles.ts
@@ -4,12 +4,32 @@ import { Colors } from "../../constants/colors";
 const createStyles = () => StyleSheet.create({
     body: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        // justifyContent: 'center',
+        // alignItems: 'center'
     },
     text: {
         fontSize: RFValue(25),
         fontWeight: '300',
+    },
+    pointsWrapper: {
+      margin: 10
+    },
+    renderItemPoint: {
+        height: 150,
+        width: 150,
+        borderWidth: 1,
+        borderColor: '#00ff00',
+        borderRadius: 20,
+        marginRight: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    avatar: {
+        paddingTop: 20,
+        height: 100,
+        width: 100,
+        borderRadius: 100,
+        padding: 20,
     }
 })
 
