42f43b400e8e6b634f62f187ac8bb3cced22cb74
Serhii Tokariev <seregat1984@gmail.com>
2021-12-28 23:32:05 +0200

    Get selling point check info

--- allcookies-tracker-backend/src/domain/activity/repo.rs
+++ allcookies-tracker-backend/src/domain/activity/repo.rs
@@ -1,6 +1,21 @@
-use crate::domain::{Activity, Count, PagedResult};
+use crate::domain::{Activity, Count, LatLonPoint, PagedResult};
 use crate::AnError;
+use serde::{Deserialize, Serialize};
 use sqlx::{Postgres, Transaction};
+
+#[derive(Serialize, Deserialize)]
+pub struct SellingPointCheckDto {
+    pub id: i64,
+    pub activity_id: i64,
+    pub selling_point_id: i64,
+    pub selling_point_title: String,
+    pub selling_point_location: Option<LatLonPoint>,
+    pub product_id: i64,
+    pub product_title: String,
+    pub product_image_url: String,
+    pub product_is_disabled: bool,
+    pub quantity: i32,
+}
 
 #[async_trait::async_trait]
 pub trait ActivityRepo {
@@ -11,7 +26,7 @@ pub trait ActivityRepo {
         current_user_id: i64,
         skip: i64,
         take: i64,
-    ) -> Result<PagedResult<Activity>, AnError>;
+    ) -> Result<(PagedResult<Activity>, Vec<SellingPointCheckDto>), AnError>;
 }
 
 pub struct PersistentActivityRepo<'a, 'c> {
@@ -59,7 +74,7 @@ impl<'a, 'c> ActivityRepo for PersistentActivityRepo<'a, 'c> {
         current_user_id: i64,
         skip: i64,
         take: i64,
-    ) -> Result<PagedResult<Activity>, AnError> {
+    ) -> Result<(PagedResult<Activity>, Vec<SellingPointCheckDto>), AnError> {
         let activity = sqlx::query_as!(
             Activity,
             r#"select
@@ -86,6 +101,44 @@ impl<'a, 'c> ActivityRepo for PersistentActivityRepo<'a, 'c> {
         .fetch_all(&mut *self.db)
         .await?;
 
+        let selling_point_check_dtos = sqlx::query_as!(
+            SellingPointCheckDto,
+            r#"
+            select
+                spc.id,
+                spc.activity_id,
+                spc.selling_point_id,
+                sp.title as "selling_point_title!",
+                sp.location as "selling_point_location: _",
+                spc.product_id,
+                p.title as "product_title!",
+                p.image_url as "product_image_url!",
+                p.is_disabled product_is_disabled,
+                spc.quantity
+            from
+                selling_point_check spc inner join
+                selling_point sp on spc.selling_point_id = sp.id inner join
+                product p on spc.product_id = p.id
+            where
+                spc.activity_id in (
+                    select 
+                        id 
+                    from
+                        activity a 
+                    where 
+                        a.created_by = $1 and a.deleted_by is null
+                    order by 
+                        a.created_at desc
+                    offset $2 limit $3
+                )            
+            "#,
+            current_user_id,
+            skip,
+            take
+        )
+        .fetch_all(&mut *self.db)
+        .await?;
+
         let count = sqlx::query_as!(
             Count,
             r#"
@@ -101,9 +154,12 @@ impl<'a, 'c> ActivityRepo for PersistentActivityRepo<'a, 'c> {
         .fetch_one(&mut *self.db)
         .await?;
 
-        Ok(PagedResult {
-            total: count.count,
-            data: activity,
-        })
+        Ok((
+            PagedResult {
+                total: count.count,
+                data: activity,
+            },
+            selling_point_check_dtos,
+        ))
     }
 }
