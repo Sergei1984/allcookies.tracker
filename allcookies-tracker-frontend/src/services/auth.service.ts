155a9cb9491758a213f046c217b655d9bcf2aa14
a33nazarenko <a33.nazarenko@gmail.com>
2022-02-09 11:23:00 +0200

    change request

--- allcookies-tracker-frontend/src/services/auth.service.ts
+++ allcookies-tracker-frontend/src/services/auth.service.ts
@@ -3,7 +3,7 @@ import { SignInData } from "../store/user/types";
 class AuthService {
 
     public signIn = async (data: SignInData) => {
-        const response = await axios.post('http://allcookies-tracker.a-dev.com/auth/sign-in', data);
+        const response = await axios.post('https://allcookies-tracker.a-dev.com/auth/sign-in', data);
         return response.data;
     }
     
