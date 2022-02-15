6c03942b186f2c8e06a4170d9207089c751ed56f
RaiSky <trololopower99@gmail.com>
2022-02-15 13:43:55 +0200

    update user activity info

--- allcookies-tracker-admin/src/components/user-activity/user-activity-products-item.tsx
+++ allcookies-tracker-admin/src/components/user-activity/user-activity-products-item.tsx
@@ -1,58 +1,167 @@
-import React, {FC} from "react";
-import Box from "@mui/material/Box";
+import React, { FC } from 'react';
+import { Box, Typography } from '@mui/material';
 
 interface UserActivityProductsItemProps {
-  src: string,
-  text: string,
-  amount: number
+  src: string;
+  text: string;
+  amount: number;
+  order_quantity: number;
+  remaining_quantity: number;
 }
 
 const UserActivityProductsItem: FC<UserActivityProductsItemProps> = ({
-                                                                       src,
-                                                                       text,
-                                                                       amount
-                                                                     }) => {
-
+  src,
+  text,
+  amount,
+  order_quantity,
+  remaining_quantity,
+}) => {
+  console.log(order_quantity, remaining_quantity);
   return (
-      <Box sx={{
-        background: "#E6E9F4",
-        width: 'calc(25% - 3px)',
+    <Box
+      sx={{
+        background: '#ffffff',
+        // width: 'calc(25% - 3px)',
+        width: 'auto',
         display: 'flex',
         alignItems: 'center',
-        px: '10px',
-        py: '8px',
+        mx: '10px',
+        my: '8px',
         borderRadius: '3px',
       }}
+    >
+      <img
+        src={src}
+        alt='Image'
+        style={{
+          height: '65px',
+          width: '65px',
+          objectFit: 'contain',
+          marginLeft: '10px',
+          marginRight: '10px',
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          borderLeft: '1px solid #D7D7D7',
+        }}
       >
-        <img
-            src={src}
-            alt="Image"
-            style={{
-              height: "50px",
-              width: "50px",
-              objectFit: "contain",
-              marginRight: "16px",
-            }}
-        />
-        <span style={{flex: 1, marginRight: '10px'}}>{text}</span>
-        <Box
+        <Box sx={{ width: '100%', borderBottom: '1px solid #D7D7D7' }}>
+          <Typography
             sx={{
-              background: "#C3E4E4",
-              borderRadius: "5px",
-              height: '37px',
-              ml: 'auto',
-              minWidth: '31px',
-              px: '5px',
-              fontSize: '18px',
-              textAlign: 'center',
-              lineHeight: '37px',
+              padding: '10px',
+              fontWeight: 'bold',
+              fontSize: '12px',
+              lineHeight: '14px',
               letterSpacing: '-0.01em',
-              color: '#767676',
+              color: '#161616',
             }}
-        >{amount}</Box>
+          >
+            {text}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: '5px',
+              borderRight: '1px solid #D7D7D7',
+            }}
+          >
+            <Typography
+              sx={{
+                color: '#4E4E4E',
+                fontSize: '11px',
+                lineHeight: '13px',
+              }}
+            >
+              Сколько осталось
+            </Typography>
+            <Typography
+              sx={{
+                background: '#D1F3F3',
+                borderRadius: '5px',
+                height: '37px',
+                ml: 'auto',
+                minWidth: '31px',
+                px: '5px',
+                fontSize: '18px',
+                textAlign: 'center',
+                lineHeight: '37px',
+                letterSpacing: '-0.01em',
+                color: '#42A6A6',
+              }}
+            >
+              {order_quantity}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: '5px',
+            }}
+          >
+            <Typography
+              sx={{
+                color: '#4E4E4E',
+                fontSize: '11px',
+                lineHeight: '13px',
+              }}
+            >
+              Сколько заказать
+            </Typography>
+            <Typography
+              sx={{
+                background: '#D1F3F3',
+                borderRadius: '5px',
+                height: '37px',
+                ml: 'auto',
+                minWidth: '31px',
+                px: '5px',
+                fontSize: '18px',
+                textAlign: 'center',
+                lineHeight: '37px',
+                letterSpacing: '-0.01em',
+                color: '#42A6A6',
+              }}
+            >
+              {remaining_quantity}
+            </Typography>
+          </Box>
+        </Box>
       </Box>
-  )
+      {/* <Box
+        sx={{
+          background: '#C3E4E4',
+          borderRadius: '5px',
+          height: '37px',
+          ml: 'auto',
+          minWidth: '31px',
+          px: '5px',
+          fontSize: '18px',
+          textAlign: 'center',
+          lineHeight: '37px',
+          letterSpacing: '-0.01em',
+          color: '#767676',
+        }}
+      >
+        {amount}
+      </Box> */}
+    </Box>
+  );
+};
 
-}
-
-export default UserActivityProductsItem
+export default UserActivityProductsItem;
